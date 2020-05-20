9b7f97a0bb2047b500678500b0c3b67ccbdc6dea
saivarsha17 <47611582+saivarsha17@users.noreply.github.com>
2020-05-20 16:23:28 +0530

    Add files via upload

---
+++ feedback/spice/ee18btech11042/spice_1.py
@@ -0,0 +1,26 @@
+#if using termux
+import subprocess
+import shlex
+#end if
+import numpy as np
+import matplotlib.pyplot as plt
+din = np.loadtxt('./spice/ee18btech11042/input.dat')
+dout = np.loadtxt('./spice/ee18btech11042/output.dat')
+plt.subplot(2,1,1)
+
+plt.ylabel('$v_{in}$')
+plt.plot(din[:,0],din[:,1])
+
+plt.subplot(2,1,2)
+plt.xlabel('$t$')
+plt.ylabel('$v_{out}$')
+plt.plot(dout[:,0],dout[:,1])
+#if using termux
+plt.savefig('./figs/ee18btech11042/ee18btech11042.pdf')
+plt.savefig('./figs/ee18btech11042/ee18btech11042_1.eps')
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11042/ee18btech11042_1.pdf"))
+#else
+#plt.show()
+
+plt.show()
+
